73dbe4f8c59c8c336f2dfb79f1040ddea728f714
Alfiya Siddique <alfiyasiddique@1708>
2023-03-12 19:35:42 +0530

    server

--- functions/api.js
+++ functions/api.js
@@ -1,12 +1,15 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('../db.json'); 
+const router = jsonServer.router('db.json'); 
+const middlewares = jsonServer.defaults();
 
 server.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   next();
 });
+server.use(middlewares);
+server.use( router); 
 
-server.use('/api', router); 
-
-module.exports = server;
+server.listen(3000, ()=>{
+    console.log("json running")
+})
