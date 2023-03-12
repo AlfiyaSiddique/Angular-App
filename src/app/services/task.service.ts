2b4ca73173a3f123f44dff2fd5ea1a6c984ffaff
Alfiya Siddique <86224794+AlfiyaSiddique@users.noreply.github.com>
2023-03-12 18:59:53 +0530

    Update task.service.ts

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -23,11 +23,11 @@ constructor(private _http: HttpClient) { }
    }else{
     localStorage.setItem("todo", JSON.stringify([data]))
    }
-        return this._http.post("http://localhost:3000/task", data)
+        return this._http.post("https://main--enchanting-marshmallow-109241.netlify.app/task", data)
   }
 
   getTaskList(): Observable<any>{
-    return this._http.get("http://localhost:3000/task")
+    return this._http.get("https://main--enchanting-marshmallow-109241.netlify.app/task")
 }
 
 deleteTask(id: Number): Observable<any>{
@@ -40,6 +40,6 @@ deleteTask(id: Number): Observable<any>{
       }
   }
   localStorage.setItem("todo", JSON.stringify(newArray))
-  return this._http.delete(`http://localhost:3000/task/${id}`)
+  return this._http.delete(`https://main--enchanting-marshmallow-109241.netlify.app/task/${id}`)
 }
 }
